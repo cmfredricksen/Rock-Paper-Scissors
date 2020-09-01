9f5102f3fb6759b0884d96cc968f51c7546da88e
Christine Fredricksen <christine@christinefredricksen.com>
2020-09-01 00:22:59 -0500

    Finished my game for now, it works and keeps tally

--- script.js
+++ script.js
@@ -1,63 +1,85 @@
-const selectionButtons = document.querySelectorAll('[data-selection]');
-const finalColumn = document.querySelector('[data-final-column]');
-const computerScoreSpan = document.querySelector('[data-computer-score]');
-const yourScoreSpan = document.querySelector('[data-your-score]');
+let userScore = 0;
+let compScore = 0;
 
-const SELECTIONS = [
-{
-name: 'rock',
-emoji: '&#x270A;',
-beats: 'scissors',
-},
-{
-name: 'paper',
-emoji: '&#x1F91A;',
-beats: 'rock',
-},
-{
-name: 'scissors',
-emoji: '&#x270C;',
-beats: 'paper',
-}
-];
+const userScore_span = document.getElementById('user-score');
+const compScore_span = document.getElementById('computer-score');
+const scoreBoard_div = document.querySelector('.score-board');
+const result_div = document.querySelector('.result');
+const rock_div = document.getElementById('r');
+const paper_div = document.getElementById('p');
+const scissors_div = document.getElementById('s');
 
-selectionButtons.forEach(selectionButton => {
-selectionButton.addEventListener('click', e => {
-const selectionName = selectionButton.dataset.selection;
-const selection = SELECTIONS.find(selection => selection.name === selectionName);
-})
-})
-
-function makeSelection(selection) {
-const computerSelection = randomSelection()
-const yourWinner = isWinner(selection, computerSelection)
-const computerWinner = isWinner(computerSelection, selection)
-
-addSelectionResult(computerSelection, computerWinner)
-addSelectionResult(selection, yourWinner)
-
-if (yourWinner) incrementScore(yourScoreSpan)
-if (computerWinner) incrementScore(computerScoreSpan)
+function getComputerChoice() {
+    const choices = ['r', 'p', 's'];
+    const randomNum = Math.floor(Math.random() * 3);
+    return choices[randomNum];
 }
 
-function incrementScore(scoreSpan) {
-scoreSpan.innerText = parseInt(scoreSpan.innerText) + 1
+// console.log(getComputerChoice());
+function convertToWord(letter) {
+    if (letter === 'r') return 'Rock';
+    if (letter === 'p') return 'Paper';
+    return 'Scissors';
 }
 
-function addSelectionResult(selection, winner) {
-const div = document.createElement('div')
-div.innerText = selection.emoji
-div.classList.add('result-selection')
-if (winner) div.classList.add('winner')
-finalColumn.after(div)
+function win(userChoice, computerChoice) {
+    userScore++;
+    userScore_span.innerHTML = userScore;
+    result_div.innerHTML = `You win! ${convertToWord(userChoice)} beats ${convertToWord(computerChoice)}`;
+
+   
 }
 
-function isWinner(selection, opponentSelection) {
-return selection.beats === opponentSelection.name
+function lose(userChoice, computerChoice) {
+    compScore++;
+    compScore_span.innerHTML = compScore;
+    result_div.innerHTML = `You lose! ${convertToWord(computerChoice)}
+    beats ${convertToWord(userChoice)}`;
+    
+    
 }
 
-function randomSelection() {
-const randomIndex = Math.floor(Math.random() * SELECTIONS.length[randomIndex]
-)}
+function tie(userChoice) {
+    result_div.innerHTML = `It's a tie! Both chose ${convertToWord(userChoice)}`
+   
+   
+}
+
+function game(userChoice) {
+    const computerChoice = getComputerChoice();
+
+    switch (userChoice + computerChoice) {
+        case 'rs':
+        case 'pr':
+        case 'sp':
+            win(userChoice, computerChoice);
+            break;
+        case 'rp':
+        case 'ps':
+        case 'sr':
+            lose(userChoice, computerChoice); 
+            break;
+        case 'rr':
+        case 'pp':
+        case 'ss':
+            tie(userChoice);  
+            break;     
+    }
+}
 
 
+
+game('c');
+
+function main() {
+    rock_div.addEventListener('click', () => game('r'));
+    
+    paper_div.addEventListener('click', () => game('p'));
+    
+    scissors_div.addEventListener('click', () => game('s'));
+}
+
+main();
+
+
+ 
